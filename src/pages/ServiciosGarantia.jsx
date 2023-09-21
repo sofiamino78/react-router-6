cda2698d3fe872e081b39d3df9b594a8ed7df914
Sofia <sofiamino78@gmail.com>
2023-09-21 20:50:24 -0300

    First commit

---
+++ src/pages/ServiciosGarantia.jsx
@@ -0,0 +1,25 @@
+export function ServiciosGarantia() {
+    return (
+        <>
+            <h2>Nuestra garantía nos respalda</h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi 
+                nulla ullam nihil? Ipsam ullam rem cumque dolorem nisi animi iste 
+                facilis incidunt? Expedita accusamus rerum fuga deserunt saepe 
+                laudantium quia!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi 
+                nulla ullam nihil? Ipsam ullam rem cumque dolorem nisi animi iste 
+                facilis incidunt? Expedita accusamus rerum fuga deserunt saepe 
+                laudantium quia!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi 
+                nulla ullam nihil? Ipsam ullam rem cumque dolorem nisi animi iste 
+                facilis incidunt? Expedita accusamus rerum fuga deserunt saepe 
+                laudantium quia!
+            </p>
+        </>
+    )
+}
---
+++ src/pages/ServiciosPoliticas.jsx
@@ -0,0 +1,25 @@
+export function ServiciosPoliticas() {
+    return (
+        <>
+            <h2>Conoce las políticas de nuestro servicios</h2>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi 
+                nulla ullam nihil? Ipsam ullam rem cumque dolorem nisi animi iste 
+                facilis incidunt? Expedita accusamus rerum fuga deserunt saepe 
+                laudantium quia!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi 
+                nulla ullam nihil? Ipsam ullam rem cumque dolorem nisi animi iste 
+                facilis incidunt? Expedita accusamus rerum fuga deserunt saepe 
+                laudantium quia!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi 
+                nulla ullam nihil? Ipsam ullam rem cumque dolorem nisi animi iste 
+                facilis incidunt? Expedita accusamus rerum fuga deserunt saepe 
+                laudantium quia!
+            </p>
+        </>
+    )
+}
